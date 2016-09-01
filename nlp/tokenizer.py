bbb220ed871c4bbe55575afce5d4b7e8c0102487
peko <peko@gasubasu.com>
2016-09-01 17:58:53 +0400

    update

--- nlp/tokenizer.py
+++ nlp/tokenizer.py
@@ -1,26 +1,40 @@
 #!/usr/bin/python
 # coding=utf-8
 
+import io
 import sys
 import codecs
 import re
 import math
 
-def tokenize(str):
-    w = re.split(r"(\s+|[\"\'%!?.,:;\)\(=]+)", str)
-    return filter(lambda a:len(a),w)  
+def tokenize(txt):
+    w = re.split(r"(\s+|[\"\'%!?.,:;\)\(=\d]+)", txt)
+    return filter(lambda a:len(a), w)  
     
-def cleanup(str):
-    str = re.sub("\n|^\s+", '',str)
-    str = re.sub("\s+", ' ', str)
-    return str
+def cleanup(txt):
+    txt = re.sub(r"\n|^\s+", '' , txt)
+    txt = re.sub(r"\s+"    , ' ', txt)
+    return txt
 
-def deurl(str):
-    return re.sub(r"http[^\s]+", "URL", str)
+def deurl(txt):
+    return re.sub(r"http[^\s]+", "URL", txt)
 
-def denick(str):
-    return re.sub(r"@[^\s]+", "NICK", str)
+def denick(txt):
+    return re.sub(r"@[^\s]+", "NICK", txt)
 
+
+def depunct(txt):
+    k = 10.0
+    n = 1.0
+    
+    # !!! ??? ... -> !3 ?3 .3 
+    for s in re.findall(r"!{2,}|\?{2,}|\.{2,}", txt):
+        l = len(s)
+        p = int ( round( math.tanh(l*n/k) *k ) )
+        c = s[0]
+        txt = txt.replace(s, " "+c+str(p)+" ", 1)
+    return txt
+     
 def desmile(txt):
 
     # k - activation power
@@ -34,7 +48,7 @@ def desmile(txt):
     for s in re.findall(sm, txt):
         l = len( re.sub(r"[^\)]+",'',s) )
         p = int ( round( math.tanh(l*n/k) *k ) )
-        txt = txt.replace(s," SP"+str(p)+" ", 1)
+        txt = txt.replace(s," )"+str(p)+" ", 1)
     
     # :3
     n = 1.0
@@ -42,7 +56,7 @@ def desmile(txt):
     for s in re.findall(sm, txt):
         l = len( re.sub(r"[^\3Зз]+",'',s) )
         p = int ( round( math.tanh(l*n/k) *k ) )
-        txt = txt.replace(s," SP"+str(p)+" ", 1)
+        txt = txt.replace(s," )"+str(p)+" ", 1)
         
     # X)
     n = 1.0
@@ -50,7 +64,7 @@ def desmile(txt):
     for s in re.findall(sm, txt):
         l = len( re.sub(r"[^\)]+",'',s) )
         p = int ( round( math.tanh(l*n/k) *k ) )
-        txt = txt.replace(s," SP"+str(p)+" ", 1)
+        txt = txt.replace(s," )"+str(p)+" ", 1)
 
     # )))
     n = 1.0
@@ -58,7 +72,7 @@ def desmile(txt):
     for s in re.findall(sm, txt):
         l = len(s)
         p = int ( round( math.tanh(l*n/k) *k ) )
-        txt = txt.replace(s," SP"+str(p)+" ", 1)
+        txt = txt.replace(s," )"+str(p)+" ", 1)
     
     # :*
     n = 1.5
@@ -74,15 +88,15 @@ def desmile(txt):
     for s in re.findall(sm, txt):
         l = len( re.sub(r"&lt;",'',s) )
         p = int ( round( math.tanh(l*n/k) *k ) )
-        txt = txt.replace(s," SP"+str(p)+" ", 1)
+        txt = txt.replace(s," )"+str(p)+" ", 1)
 
-    # :(
+    # :( =/
     n = 1.0
-    sm = r"[:;=]-*\(+"
+    sm = r"[:;=]-*[\(/]+"
     for s in re.findall(sm, txt):
-        l = len( re.sub(r"[^\(]+",'',s) )
+        l = len( re.sub(r"[^\(/]+",'',s) )
         p = int ( round( math.tanh(l*n/k) *k ) )
-        txt = txt.replace(s," SN"+str(p)+" ", 1)
+        txt = txt.replace(s," ("+str(p)+" ", 1)
  
     # X(
     n = 1.5
@@ -90,7 +104,7 @@ def desmile(txt):
     for s in re.findall(sm, txt):
         l = len( re.sub(r"[^\(]+",'',s) )
         p = int ( round( math.tanh(l*n/k) *k ) )
-        txt = txt.replace(s," SN"+str(p)+" ", 1)
+        txt = txt.replace(s," ("+str(p)+" ", 1)
     
     # ((
     n = 1.0
@@ -98,7 +112,7 @@ def desmile(txt):
     for s in re.findall(sm, txt):
         l = len(s)
         p = int ( round( math.tanh(l*n/k) *k ) )
-        txt = txt.replace(s," SN"+str(p)+" ", 1)
+        txt = txt.replace(s," ("+str(p)+" ", 1)
     
     # :D
     n = 2.0
@@ -106,7 +120,7 @@ def desmile(txt):
     for s in re.findall(sm, txt):
         l = len( re.sub(r"[^D]+",'', s) )
         p = int ( round( math.tanh(l*n/k) *k ) )
-        txt = txt.replace(s," SP"+str(p)+" ", 1)
+        txt = txt.replace(s," )"+str(p)+" ", 1)
     
     # :C
     n = 2.0
@@ -114,7 +128,7 @@ def desmile(txt):
     for s in re.findall(sm, txt):
         l = len( re.sub(u"[^CСсc]+",'', s) )
         p = int ( round( math.tanh(l*n/k) *k ) )
-        txt = txt.replace( s," SN"+str(p)+" ", 1)
+        txt = txt.replace( s," ("+str(p)+" ", 1)
     
     # ^_^
     n = 1.0
@@ -122,56 +136,53 @@ def desmile(txt):
     for s in re.findall(sm, txt):
         l = len( re.sub(r"[^_]+",'', s) )
         p = int ( round( math.tanh(l*n/k) *k ) )
-        txt = txt.replace(s," SP"+str(p)+" ", 1)
+        txt = txt.replace(s," )"+str(p)+" ", 1)
     
     # T_T 
     n = 2.0
-    sm = u"[TТ]_*[TТ]"
+    sm = u"[TТ]_+[TТ]"
     for s in re.findall(sm, txt):
         l = len( re.sub(r"[^_]+",'', s) )
         p = int ( round( math.tanh(l*n/k) *k ) )
-        txt = txt.replace(s," SN"+str(p)+" ", 1)
+        txt = txt.replace(s," ("+str(p)+" ", 1)
     
     # *_*
     n = 1.5
-    sm = r"\*_*\*"
+    sm = r"\*_+\*"
     for s in re.findall(sm, txt):
         l = s.count("_")
         p = int ( round( math.tanh(l*n/k) *k ) )
-        txt = txt.replace(s," SP"+str(p)+" ", 1)
+        txt = txt.replace(s," )"+str(p)+" ", 1)
 
     # >_<
     n = 1.5
-    sm = r"&gt;_*&lt;"
+    sm = r"&gt;_+&lt;"
     for s in re.findall(sm, txt):
         l = s.count("_")
         p = int ( round( math.tanh(l*n/k) *k ) )
-        txt = txt.replace(s," SP"+str(p)+" ", 1)
+        txt = txt.replace(s," )"+str(p)+" ", 1)
     
     # o_o
-    n = 0.5
-    sm = u"[OoОо0]_*[OoОо0]"
+    n = 1.0
+    sm = u"[OoОо0]_+[OoОо0]"
     for s in re.findall(sm, txt):
         l = len( re.sub(r"[^_]+",'', s) )
         p = int ( round( math.tanh(l*n/k) *k ) )
-        txt = txt.replace(s," SN"+str(p)+" ", 1)
+        txt = txt.replace(s," ("+str(p)+" ", 1)
     
-    
-    # single, not paired ( )
-    if txt.count("(") != txt.count(")"):
-        txt = txt.replace("(", " SN1 ").replace(")", " SP1 ")
-        
     return txt
 
 def deemoji(txt):
     
+    k = 10.0
+    
     # 😉
     n = 1.0
-    sm = u"[😉😊😋😌😍😏😘😚😜😝😻😼😽☺♥⭐🎉💋💓💕💖😇😈😎😗😙😛]+"
+    sm = u"[❤😉😊😋😌😍😏😘😚😜😝😻😼😽☺♥⭐🎉💋💓💕💖😇😈😎😗😙😛]+"
     for s in re.findall(sm, txt):
         l = len(s)
         p = int ( round( math.tanh(l*n/k) *k ) )
-        txt = txt.replace(s," SP"+str(p)+" ", 1)
+        txt = txt.replace(s," )"+str(p)+" ", 1)
     
     # 😁
     n = 2.0
@@ -179,7 +190,7 @@ def deemoji(txt):
     for s in re.findall(sm, txt):
         l = len(s)
         p = int ( round( math.tanh(l*n/k) *k ) )
-        txt = txt.replace(s," SP"+str(p)+" ", 1)
+        txt = txt.replace(s," )"+str(p)+" ", 1)
     
     # 😔
     n =1.0
@@ -187,7 +198,7 @@ def deemoji(txt):
     for s in re.findall(sm, txt):
         l = len(s)
         p = int ( round( math.tanh(l*n/k) *k ) )
-        txt = txt.replace(s," SN"+str(p)+" ", 1)
+        txt = txt.replace(s," ("+str(p)+" ", 1)
     
     # 😩
     n = 2.0   
@@ -195,8 +206,10 @@ def deemoji(txt):
     for s in re.findall(sm, txt):
         l = len(s)
         p = int ( round( math.tanh(l*n/k) *k ) )
-        txt = txt.replace(s," SN"+str(p)+" ", 1)
+        txt = txt.replace(s," ("+str(p)+" ", 1)
 
+    return txt
+    
 def dedigit(txt):
     return re.sub(r"([-+]*[0-9]+[-:.,хx/]*)+", " DGT ", txt)
 
@@ -205,16 +218,15 @@ def dehash(txt):
 
 if __name__ == "__main__":
     
-    stdin = codecs.getreader("utf-8")(sys.stdin)
-
+    # stdin = codecs.getreader("utf-8")(sys.stdin)
+    stdin = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
     for l in stdin:
-        print("-"*10)
     
-        l = cleanup(l)
-        
+        # l = cleanup(l)
         s = deurl(l)
         s = denick(s)
         s = dedigit(s)
+        s = depunct(s)
         s = desmile(s)
         s = deemoji(s)
         
@@ -222,8 +234,7 @@ if __name__ == "__main__":
         s = cleanup(s)
         
         w = tokenize(s)
-        
+        print("-"*40)
         print(l)
-        print(s)
-        print("|".join(w).encode("utf-8"))
+        print("|".join(w))
         
---
+++ nlp/twitter-stream.py
@@ -0,0 +1,53 @@
+#!./env/bin/python
+# coding=utf-8
+
+from TwitterAPI import TwitterAPI
+from config import twitter as cfg
+import hashlib
+import redis
+
+import tokenizer as tk
+
+rdb = redis.StrictRedis(host='localhost', port=5678, db=0)
+
+api = TwitterAPI(
+    cfg['consumer_key'       ], 
+    cfg['consumer_secret'    ], 
+    cfg['access_token_key'   ], 
+    cfg['access_token_secret'])
+
+# query = {'location': '37.363697,55.512489,37.870326,55.969110'}
+query = {'track': u"и,не,я,в,а,на,с,у,как,ты,меня,так,мне,все,но,ну,по,то,за,да,вот,уже,сегодня,тебя,же,бы,ещё,мы,к"}
+stream = api.request('statuses/filter', query)
+def md5(t):
+    return hashlib.md5(t.encode('utf-8')).hexdigest()
+
+def save_tweet(t):
+    
+    m = md5(t)
+    has = rdb.sismember("filter", m)
+    if has == 1:
+        print("// REPEAT //")
+        return
+ 
+    
+    t = tk.deurl(t)
+    t = tk.denick(t)
+    t = tk.dedigit(t)
+    t = tk.desmile(t)
+    t = tk.deemoji(t)
+    t = tk.depunct(t)
+    t = tk.cleanup(t)
+    l = tk.tokenize(t)
+
+    rdb.rpush('tweets', t)
+    rdb.sadd('filter', m)
+    
+    for w in l:
+       rdb.zincrby('word_stat', w, 1)
+       
+    print(t)
+     
+for tweet in stream:
+    if 'text' in tweet:
+        save_tweet(tweet['text'])        
