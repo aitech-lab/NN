b66d2c07485998ec411990773a877d95df1735a4
peko <gasu.basu@gmail.com>
2016-09-14 17:31:15 +0300

    update

--- sentiments/encode-pipe.py
+++ sentiments/encode-pipe.py
@@ -13,5 +13,6 @@ vocabulary.init()
 stdin = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
 for l in stdin:
     (tone, codes) = vocabulary.encode(l)
-    print(tone, end="\t")
-    print("\t".join(str(c) for c in codes))
+    if(abs(tone)>0.2):
+        print('{:3.1f}'.format(tone), end="\t")
+        print("\t".join(str(c) for c in codes))
