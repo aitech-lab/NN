785c8b6740ff721792285fc279ff0e52ebdabd62
peko <gasu.basu@gmail.com>
2016-09-16 17:00:27 +0300

    update

--- sentiments/python/predict.py
+++ sentiments/python/predict.py
@@ -24,8 +24,8 @@ x = sequence.pad_sequences(x, maxlen=40)
 results = model.predict_proba(x)
 for i, v in enumerate(results):
     print(
-        "{:3.2}".format(0.5+encoded[i][0]/20.0)+"\t"+
-        voc.decode(ecoded[i][1]+"\t"+
-        "{:3.2}".format(v[0])+"\t"+
+        "{:3.2}".format(0.5+encoded[i][0]/20.0)+"\t"
+        voc.decode(ecoded[i][1]+"\t"
+        "{:3.2}".format(v[0])+"\t"
         tweets[i], 
     end="")
