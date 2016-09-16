933c09ea063948a2ef48bc822a1c96acf7849abf
peko <peko@gasubasu.com>
2016-09-16 05:19:06 +0400

    update

--- sentiments/python/predict.py
+++ sentiments/python/predict.py
@@ -1,6 +1,12 @@
 #!./env/bin/python3
 # codding=utf-8
 
+import sys
+
+if len(sys.argv) != 3:
+    print("Usage:\n\tpredict.py model.h5 data.txt")
+    sys.exit(0)
+    
 from keras.models import load_model
 from keras.preprocessing import sequence
 import numpy as np
@@ -9,14 +15,12 @@ from slang import cleanup
 import vocabulary as voc
 voc.init()
 
-if len(sys.argv) != 3:
-    print("Usage:\n\tpredict.py model.h5 data.txt")
-
 model = load_model(sys.argv[1])
-tweets = [t for t in open(sys.argv[2], encoding="utf-8")]
-x = np.array([voc.encode(t[0]) for t in tweets])
+tweets  = [t for t in open(sys.argv[2], encoding="utf-8")]
+encoded = [voc.encode(t) for t in tweets]
+x = np.array([e[1][0:40] for e in encoded])
 x = sequence.pad_sequences(x, maxlen=40)
 
 results = model.predict_proba(x)
 for i, v in enumerate(results):
-    print(tweets[i][1], "\t", v[0], "\t", tweets[i][0])
+    print(encoded[i][0], "\t", v[0], "\t", tweets[i], end="")
