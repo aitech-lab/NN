c176b058407483494871d19b6ca28575aa2b7bb2
peko <peko@gasubasu.com>
2016-10-14 03:14:03 +0400

    update

--- mediametrics/data/download.sh
+++ mediametrics/data/download.sh
@@ -2,6 +2,6 @@
 
 for i in {07..10}; do
     for j in {01..31}; do
-        axel -an10 "http://mediametrics.ru/data/archive/hour/ru-2016-${i}-${j}.zip"
+        axel -an10 "http://mediametrics.ru/data/archive/day/ru-2016-${i}-${j}.zip"
     done
 done
